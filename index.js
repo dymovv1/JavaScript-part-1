6d65aa92be08abd68849427bacdeb3c98d0232bb
dymovv1 <romandymovv1997@gmail.com>
2023-04-07 12:39:01 +0200

    homework js1

---
+++ index.js
@@ -0,0 +1,24 @@
+let name = prompt ("Your Name?");
+console.log (name)
+
+let age = prompt (name + " Your year of birth?");
+let resualtAge = ("2023");
+console.log (resualtAge - age);
+
+let side = prompt ('Say me random length of square?');
+let perimeter = 4 * side;
+
+let resualtSquare = "Your square = " + perimeter;
+alert(resualtSquare);
+
+console.log (side = perimeter);
+
+const ok = confirm ("Is it ok?")
+
+const dollar = prompt ("How many dollars do you have?")
+const euro = dollar * 0.92;
+
+const total = "You have in Euro =" + euro;
+prompt(total);
+
+console.log (total);
